8a1fe0141bc079192fb9aa56653b18fbcd88fb0f
tushar994 <54453857+tushar994@users.noreply.github.com>
2024-10-14 12:30:29 +0530

    add email:read scope

--- start.go
+++ start.go
@@ -97,7 +97,7 @@ func (a *application) getLoginURL(c echo.Context, flowType, loginType, st string
 		options = append(options, oauth2.SetAuthURLParam("client_id", "random"))
 		options = append(options, oauth2.SetAuthURLParam("code_challenge_method", "S256"))
 		options = append(options, oauth2.SetAuthURLParam("code_challenge", challenge))
-		options = append(options, oauth2.SetAuthURLParam("scope", "board:play challenge:write challenge:read bot:play"))
+		options = append(options, oauth2.SetAuthURLParam("scope", "board:play challenge:write challenge:read bot:play email:read"))
 		session.Values["codeVerifier"] = verifier
 		session.Save(c.Request(), c.Response().Writer)
 	}
