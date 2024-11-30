8d8b1d3cd1557f0eb35ae6e3c600d78cf3a4ef76
startup-dreamer <prsumit35@gmail.com>
2024-11-30 08:45:58 +0530

    refactor: the /complete endpoint

--- complete.go
+++ complete.go
@@ -2,14 +2,10 @@ package main
 
 import (
 	"context"
-	"errors"
-	"fmt"
 	"net/http"
 	"strings"
-	"time"
 
 	"github.com/go-jose/go-jose/v4"
-	"github.com/go-jose/go-jose/v4/jwt"
 	"github.com/labstack/echo/v4"
 	"golang.org/x/oauth2"
 )
@@ -21,50 +17,37 @@ type completeParams struct {
 
 func (a *application) completeLogin(c echo.Context) error {
 	c.Response().Header().Set("Access-Control-Allow-Credentials", "true")
-	fmt.Println("complete login called")
 
-	headers := c.Request().Header
-	for name, values := range headers {
-		for _, value := range values {
-			fmt.Println("%s: %s", name, value)
-		}
-	}
 	params := new(completeParams)
 	if err := c.Bind(params); err != nil {
-		fmt.Println(err)
-		return err
+		return echo.NewHTTPError(http.StatusBadRequest, "invalid parameters")
 	}
 
 	stateB64 := params.State
 	code := params.Code
 	if stateB64 == "" || code == "" {
-		fmt.Println("Invalid query params")
-		return errors.New("invalid query params")
+		return echo.NewHTTPError(http.StatusBadRequest, "invalid query params")
 	}
 
 	ss, err := jose.ParseSigned(stateB64, []jose.SignatureAlgorithm{jose.ES256})
 	if err != nil {
-		fmt.Println("could not parse login state")
 		return echo.NewHTTPError(http.StatusForbidden, "could not parse login state")
 	}
 
 	verifiedData, err := ss.Verify(&a.publicKey)
 	if err != nil {
-		fmt.Println("could not verify login state")
 		return echo.NewHTTPError(http.StatusForbidden, "could not verify login state")
 	}
 
 	state := string(verifiedData)
 	sl := strings.Split(state, ":")
 	if len(sl) < 3 {
-		fmt.Println("unexpected state found")
-		return errors.New("unexpected state found")
+		return echo.NewHTTPError(http.StatusBadRequest, "unexpected state found")
 	}
 
 	p, ok := a.authMap[sl[1]]
 	if !ok {
-		fmt.Println("login type not available")
-		return errors.New("login type not available")
+		return echo.NewHTTPError(http.StatusBadRequest, "login type not available")
 	}
 
 	session, _ := Store.Get(c.Request(), "cookie-name")
@@ -72,43 +55,32 @@ func (a *application) completeLogin(c echo.Context) error {
 	var oauth2Token *oauth2.Token
 	if !ok || verifier == "" {
 		oauth2Token, err = p.conf.Exchange(context.Background(), code)
-		if err != nil {
-			fmt.Println("something: %s", err.Error())
-			return err
-		}
 	} else {
 		oauth2Token, err = p.conf.Exchange(context.Background(), code, oauth2.SetAuthURLParam("code_verifier", verifier))
-		if err != nil {
-			fmt.Println("something: %s", err.Error())
-			return err
-		}
+	}
+	if err != nil {
+		return echo.NewHTTPError(http.StatusInternalServerError, "failed to exchange token")
 	}
 
 	accessToken := oauth2Token.AccessToken
 	id, err := p.conf.getUserInfo(accessToken)
 	if err != nil {
-		fmt.Println("err:", err)
-		return err
+		return echo.NewHTTPError(http.StatusInternalServerError, "failed to get user info")
 	}
 
 	if sl[0] == "link" {
-		userID, err := a.db.GetUserID(id, sl[1])
-		fmt.Println("getting current userID")
+		showdownUserID, err := a.db.GetUserID(id, sl[1])
 		if err != nil {
 			return err
 		}
-		fmt.Println("userID")
-		fmt.Println(userID)
-		if userID != "" {
-			fmt.Println("This account is already linked")
-			return c.String(http.StatusBadRequest, "This account already exists")
+		if showdownUserID != "" {
+			return echo.NewHTTPError(http.StatusBadRequest, "This account already exists")
 		}
 	}
 
 	if sl[1] == "lichess" {
 		err = a.db.CreateLichessToken(id, accessToken)
 		if err != nil {
-			fmt.Println(err)
 			return err
 		}
 	}
@@ -116,82 +88,58 @@ func (a *application) completeLogin(c echo.Context) error {
 	if sl[0] == "link" {
 		err := a.linkComplete(id, sl[1], sl[2])
 		if err != nil {
-			fmt.Println(err)
 			return err
 		}
 	}
-	// Create JWT
-	cl := jwt.Claims{
-		Audience:  []string{a.jwtAudience},
-		Issuer:    a.selfURL,
-		NotBefore: jwt.NewNumericDate(time.Now()),
-		IssuedAt:  jwt.NewNumericDate(time.Now()),
-		Expiry:    jwt.NewNumericDate(time.Now().Add(time.Minute * 3)),
-	}
 
-	// Get user from DB
-	user, err := a.db.GetUserID(id, sl[1])
+	showdownUserID, err := a.db.GetUserID(id, sl[1])
 	if err != nil {
-		fmt.Println("1")
-		fmt.Println(err)
 		return err
 	}
-	if user == "" {
+
+	if showdownUserID == "" {
 		err := a.db.CreateNewUser(id, sl[1])
 		if err != nil {
-			fmt.Println("2")
-			fmt.Println(err)
+			return err
+		}
+		showdownUserID, err = a.db.GetUserID(id, sl[1])
+		if err != nil {
 			return err
 		}
 	}
-	user, err = a.db.GetUserID(id, sl[1])
+
+	accounts, err := a.db.GetConnectedAccounts(showdownUserID)
 	if err != nil {
-		fmt.Println("3")
-		fmt.Println(err)
 		return err
 	}
-	secondID := ""
-	accounts, err := a.db.GetConnectedAccounts(user)
-	if err != nil {
-		fmt.Println(err)
-		return err
-	}
-	for i := range accounts {
-		account := accounts[i]
-		if account.Provider != sl[1] {
-			secondID = account.ID
+
+	var steamID, lichessID, telegramID string
+	for _, account := range accounts {
+		switch account.Provider {
+		case "steam":
+			steamID = account.ID
+		case "lichess":
+			lichessID = account.ID
+		case TELEGRAM_PROVIDER:
+			telegramID = account.ID
 		}
 	}
 
-	telegramID, err := a.db.GetLinkedTelegramIDFromShowdownID(user)
+	accessToken, loginToken, err := a.generateShowdownAuthTokens(
+		showdownUserID,
+		"", // address
+		"", // email
+		steamID,
+		lichessID,
+		telegramID,
+	)
 	if err != nil {
-		fmt.Println("Error fetching Telegram ID:", err)
-		return err
+		return echo.NewHTTPError(http.StatusInternalServerError, "failed to generate tokens")
 	}
 
-	customClaims := customClaims{
-		UserID:     user,
-		LoginType:  sl[1],
-		LoginID:    id,
-		LinkedID:   secondID,
-		TelegramID: telegramID,
-	}
-
-	token, err := jwt.Signed(a.signer).Claims(cl).Claims(customClaims).Serialize()
-	if err != nil {
-		fmt.Println("jwtcreationerror:", err)
-		return echo.NewHTTPError(http.StatusInternalServerError)
-	}
-
-	cl.Audience = []string{cl.Issuer}
-	loginToken, err := jwt.Signed(a.signer).Claims(cl).Claims(customClaims).Serialize()
-	if err != nil {
-		fmt.Println("jwtcreationerror:", err)
-		return echo.NewHTTPError(http.StatusInternalServerError)
-	}
 	return c.JSON(http.StatusOK, map[string]string{
-		"token":      token,
-		"userID":     user,
+		"token":      accessToken,
+		"userID":     showdownUserID,
 		"loginType":  sl[1],
 		"loginToken": loginToken,
 	})
